8ad55dddc2cb9f4ac4c0652514f22a2d1b7ae73c
mansib-uddin101 <drivemansib@gmail.com>
2026-03-05 05:46:55 +0600

    Done most of cards placement logic

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-active = ["bg-blue-500", "text-white"]
-inactive = ["bg-white", "text-black"]
+const active = ["bg-blue-500", "text-white"]
+const inactive = ["bg-white", "text-black"]
+
+const allSection = document.getElementById("all-section")
+const interviewSection = document.getElementById("interview-section")
+const rejectedSection = document.getElementById("rejected-section")
+
+const totalStatus = document.getElementById("totalStatus")
+const interviewStatus = document.getElementById("interviewStatus")
+const rejectedStatus = document.getElementById("rejectedStatus")
 
 function tabSwitch(tab) {
     tabs = ["tab-all", "tab-interview", "tab-rejected"]
@@ -15,11 +23,52 @@ function tabSwitch(tab) {
         else {
             tabElement.classList.remove(...active);
             tabElement.classList.add(...inactive);
-            console.log(t);
             
         }
     }
-
-    console.log(tabElement);
-
+    const pages = [allSection, interviewSection, rejectedSection]
+    for(page of pages){
+        page.classList.add("hidden")
+    }
+    if(tab==="tab-all"){
+        allSection.classList.remove("hidden")
+    }
+    else if(tab==="tab-interview"){
+        interviewSection.classList.remove("hidden")
+    }
+    else if(tab==="tab-rejected"){
+        rejectedSection.classList.remove("hidden")
+    }
 }
+
+document.getElementById("jobs-section").addEventListener("click", function (event){
+    const clickedElement = event.target;
+    const card = clickedElement.closest(".card");
+    const status = card.querySelector(".status");
+    parent = card.parentNode;
+
+
+    if(clickedElement.classList.contains("interview")){
+        interviewSection.appendChild(card);
+        status.innerText = "INTERVIEWED";
+        updateStatus();
+    }
+    else if(clickedElement.classList.contains("rejected")){
+        rejectedSection.appendChild(card);
+        status.innerText = "REJECTED";
+        
+        updateStatus();
+    }
+    else if(clickedElement.classList.contains("delete")){
+        parent.removeChild(card);
+        updateStatus();
+    }
+})
+
+function updateStatus(){
+    console.log(rejectedStatus.innerText);
+    totalStatus.innerText = allSection.children.length;
+    interviewStatus.innerText = interviewSection.children.length;
+    rejectedStatus.innerText = rejectedSection.children.length;
+}
+updateStatus();
