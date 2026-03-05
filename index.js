45f3e1020e679321caa314a3614d0c551c6c7a06
mansib-uddin101 <drivemansib@gmail.com>
2026-03-05 06:48:09 +0600

    finished additional logic and added more cards

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+let currentTab = "tab-all"
 const active = ["bg-blue-500", "text-white"]
 const inactive = ["bg-white", "text-black"]
 
 const allSection = document.getElementById("all-section")
 const interviewSection = document.getElementById("interview-section")
 const rejectedSection = document.getElementById("rejected-section")
+const noJobs = document.getElementById("no-jobs")
 
 const totalStatus = document.getElementById("totalStatus")
 const interviewStatus = document.getElementById("interviewStatus")
 const rejectedStatus = document.getElementById("rejectedStatus")
+const availableStatus = document.getElementById("available-jobs")
 
 function tabSwitch(tab) {
+    currentTab = tab;
     tabs = ["tab-all", "tab-interview", "tab-rejected"]
     
     for (const t of tabs) {
@@ -31,14 +35,18 @@ function tabSwitch(tab) {
         page.classList.add("hidden")
     }
     if(tab==="tab-all"){
-        allSection.classList.remove("hidden")
+        allSection.classList.remove("hidden");
+        
     }
     else if(tab==="tab-interview"){
-        interviewSection.classList.remove("hidden")
+        interviewSection.classList.remove("hidden");
+        
     }
     else if(tab==="tab-rejected"){
-        rejectedSection.classList.remove("hidden")
+        rejectedSection.classList.remove("hidden");
+        
     }
+    updateStatus();
 }
 
 document.getElementById("jobs-section").addEventListener("click", function (event){
@@ -50,12 +58,12 @@ document.getElementById("jobs-section").addEventListener("click", function (even
 
     if(clickedElement.classList.contains("interview")){
         interviewSection.appendChild(card);
-        status.innerText = "INTERVIEWED";
+        status.innerText = "Interview";
         updateStatus();
     }
     else if(clickedElement.classList.contains("rejected")){
         rejectedSection.appendChild(card);
-        status.innerText = "REJECTED";
+        status.innerText = "Rejected";
         
         updateStatus();
     }
@@ -66,9 +74,37 @@ document.getElementById("jobs-section").addEventListener("click", function (even
 })
 
 function updateStatus(){
-    console.log(rejectedStatus.innerText);
     totalStatus.innerText = allSection.children.length;
     interviewStatus.innerText = interviewSection.children.length;
     rejectedStatus.innerText = rejectedSection.children.length;
+
+    if(currentTab==="tab-all"){
+        availableStatus.innerText = allSection.children.length;
+        if(allSection.children.length <1){
+            noJobs.classList.remove("hidden")
+        }
+        else{
+            noJobs.classList.add("hidden");
+        }
+        
+    }
+    else if(currentTab==="tab-interview"){
+        availableStatus.innerText = interviewSection.children.length;
+        if(interviewSection.children.length <1){
+            noJobs.classList.remove("hidden")
+        }
+        else{
+            noJobs.classList.add("hidden");
+        }
+    }
+    else if(currentTab==="tab-rejected"){
+        availableStatus.innerText = rejectedSection.children.length;
+        if(rejectedSection.children.length <1){
+            noJobs.classList.remove("hidden")
+        }
+        else{
+            noJobs.classList.add("hidden");
+        }
+    }
 }
 updateStatus();
